cb00e42ea9ce6d3b215cd92bfaf963520208f63f
WilliamLinA <ylin985@ucsb.edu>
2025-11-03 09:00:57 -0800

    Create card template

---
+++ Assets/Scripts/ElementType.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/// <summary>
+/// Defined elemental types for cards and energy
+/// </summary>
+public enum ElementType
+{
+    None = 0,   /// No specific element
+    Fire = 1,   /// Fire element
+    Earth = 2,  /// Earth element
+    Water = 3,  /// Water element
+    Air = 4,    /// Air element
+}
