1da6f234eb5ed9dc00e3bf843e4afcf138d42e9d
Mike Jiang <mike264583@gmail.com>
2025-11-02 16:59:23 -0800

    Changed CraftingEssenceCost to a List instead of an int

--- Assets/Scripts/CraftingRecipeTemplate.cs
+++ Assets/Scripts/CraftingRecipeTemplate.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "CraftingRecipeTemplate", menuName = "Scriptable Objects/CraftingRecipeTemplate")]
 public class CraftingRecipeTemplate : ScriptableObject
 {
     public string RecipeName;
-    public int CraftingEssenceCost;
+    public List<int> CraftingEssenceCost; // [0] = Fire, [1] = Earth, [2] = Water, [3] = Air
     public Card CraftingResult;
 }
